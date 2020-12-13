86bc65be8851b239673e12f5de6dde666d35f1b9
Jin-hw <70885954+Jin-hw@users.noreply.github.com>
2020-12-13 23:57:07 +0900

    Add ScreenBtn

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -41,7 +41,7 @@ export const postUpload = async (req, res) => {
         description,
         creator: req.user.id
     });
-    req.user.video.push(newVideo.id);
+    req.user.videos.push(newVideo.id);
     req.user.save();
     res.redirect(routes.videoDetail(newVideo.id));
 };
@@ -51,8 +51,7 @@ export const videoDetail = async (req, res) => {
         params: { id }
     } = req;
     try {
-        const video = await Video.findById(id);
-        console.log(video);
+        const video = await Video.findById(id).populate("creator");
         res.render("videoDetail", { pageTitle: video.title, video });
     } catch (error) {
         res.redirect(routes.home);
@@ -65,11 +64,15 @@ export const getEditVideo = async (req, res) => {
     } = req;
     try {
         const video = await Video.findById(id);
-        res.render("editVideo", { pageTitle: `Edit ${video.title}`, video })
+        if (video.creator !== req.user.id) {
+            throw Error();
+        } else {
+            res.render("editVideo", { pageTitle: `Edit ${video.title}`, video });
+        }
     } catch (error) {
         res.redirect(routes.home);
     }
-}
+};
 export const postEditVideo = async (req, res) => {
     const {
         params: { id },
@@ -88,7 +91,12 @@ export const deleteVideo = async (req, res) => {
         params: { id }
     } = req;
     try {
-        await Video.findOneAndDelete({ _id: id });
+        const video = await Video.findById(id);
+        if (video.creator !== req.user.id) {
+            throw Error();
+        } else {
+            await Video.findOneAndDelete({ _id: id });
+        }
     } catch (error) {
         console.log(error);
     }
